9d602664bbe4456f3c8cc975aa3b315adceffabc
j218group <3355844@gmail.com>
2018-12-18 22:01:58 +0200

    Products DAO

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import dao.DaoProduct;
+import models.Product;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Product product = new Product();
+
+        product.setName("Apple");
+        product.setPrice(1200.00);
+        product.setAmount(1.0);
+        product.setDescription("XS");
+
+        DaoProduct.addProduct(product);
+    }
+}
--- src/dao/DaoProduct.java
+++ src/dao/DaoProduct.java
@@ -3,18 +3,22 @@ package dao;
 import models.Product;
 import utils.Connector;
 
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DaoProduct {
 
-    public void addProduct(Product product) {
-        String query = "INSERT INTO mydb.products (POINT_ID, PRICE, AMOUNT, NAME , DESCRIPTION) VALUES" +
-                " ()";
+    public static void addProduct(Product product) {
+        String query = "INSERT INTO mydb.products (POINT_ID, PRICE, AMOUNT, NAME , DESCRIPTION) VALUES (?,?,?,?,?)";
 
         try {
-            Statement statement = Connector.getConnection().createStatement();
-            statement.execute(query);
+            PreparedStatement ps = Connector.getConnection().prepareStatement(query);
+            ps.setLong(1, product.getPointId());
+            ps.setDouble(2, product.getPrice());
+            ps.setDouble(3, product.getAmount());
+            ps.setString(4, product.getName());
+            ps.setString(5,product.getDescription());
+            ps.executeQuery();
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
